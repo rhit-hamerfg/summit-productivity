6b7096dfb2de91520993eb707568f47336db092b
rhit-hamerfg <hamerfg@rose-hulman.edu>
2024-02-08 22:34:33 -0500

    complete CSS makeover implementing grid templates into each page and a refactoring of code to provide for easy navigation

--- Summit/public/scripts/main.js
+++ Summit/public/scripts/main.js
@@ -12,23 +12,23 @@ var rhit = rhit || {};
 
 
 
+rhit.fbAuthManager = class {
+	constructor() {
+		this._user = null;
+	}
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			changeListener();
+		})
+	}
+}
 
 rhit.LoginPageController = class {
 	constructor() {
 
 		this._user = null;
 		this._unsubscribe = null;
-
-		this._unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-			if (user) {
-				const uid = user.uid;
-				console.log("Logged in", uid);
-				window.location.href = '/home.html';
-			}
-			if (this._unsubscribe) {
-				this._unsubscribe();
-			}
-		})
 
 		const inputEmail = document.querySelector("#inputEmail");
 		const inputPassword = document.querySelector("#inputPassword");
@@ -39,6 +39,7 @@ rhit.LoginPageController = class {
 				this._user = user;
 				console.log(this._user);
 				console.log(this.isSignedIn());
+				rhit.checkForRedirects(null);
 			});
 		}
 
@@ -58,28 +59,130 @@ rhit.LoginPageController = class {
 }
 
 rhit.HomePageController = class {
-    constructor() {
-        firebase.auth().onAuthStateChanged((user) => {
-            if (!user) {
-                console.log("User not signed in, redirecting to login page");
-                window.location.href = '/login.html'; // Adjust the path as necessary
-            }
-        });
+	constructor() {
+		document.querySelector("#todoButton").onclick = (event) => {
+			console.log("Going to TODO page");
+			rhit.checkForRedirects("todo");
+			rhit.initalizePage();
+		};
+		document.querySelector("#goalsButton").onclick = (event) => {
+			console.log("Going to GOALS page");
+			rhit.checkForRedirects("goals");
+			rhit.initalizePage();
+		};
+		document.querySelector("#habitsButton").onclick = (event) => {
+			console.log("Going to HABITS page");
+			rhit.checkForRedirects("habits");
+			rhit.initalizePage();
+		};
+		document.querySelector("#summitLogoText").onclick = (event) => {
+			console.log("Going to HOME page");
+			rhit.checkForRedirects("home");
+			rhit.initalizePage();
+		};
+	}
 
-        // Other home page initialization code...
-    }
+}
+
+rhit.TodoPageController = class {
+	constructor() {
+		document.querySelector("#todoButton").onclick = (event) => {
+			console.log("Going to TODO page");
+			rhit.checkForRedirects("todo");
+			rhit.initalizePage();
+		};
+		document.querySelector("#goalsButton").onclick = (event) => {
+			console.log("Going to GOALS page");
+			rhit.checkForRedirects("goals");
+			rhit.initalizePage();
+		};
+		document.querySelector("#habitsButton").onclick = (event) => {
+			console.log("Going to HABITS page");
+			rhit.checkForRedirects("habits");
+			rhit.initalizePage();
+		};
+		document.querySelector("#summitLogoText").onclick = (event) => {
+			console.log("Going to HOME page");
+			rhit.checkForRedirects("home");
+			rhit.initalizePage();
+		};
+	}
+
+}
+
+rhit.GoalsPageController = class {
+	constructor() {
+		document.querySelector("#todoButton").onclick = (event) => {
+			console.log("Going to TODO page");
+			rhit.checkForRedirects("todo");
+			rhit.initalizePage();
+		};
+		document.querySelector("#goalsButton").onclick = (event) => {
+			console.log("Going to GOALS page");
+			rhit.checkForRedirects("goals");
+			rhit.initalizePage();
+		};
+		document.querySelector("#habitsButton").onclick = (event) => {
+			console.log("Going to HABITS page");
+			rhit.checkForRedirects("habits");
+			rhit.initalizePage();
+		};
+		document.querySelector("#summitLogoText").onclick = (event) => {
+			console.log("Going to HOME page");
+			rhit.checkForRedirects("home");
+			rhit.initalizePage();
+		};
+	}
+
 }
 
 
-rhit.checkForRedirects = function () {
-	if (document.querySelector("#loginPage") && rhit.LoginPageController.isSignedIn()) {
+rhit.checkForRedirects = function (buttonPressed) {
+	if (document.querySelector(".loginPage") && rhit.LoginPageController.isSignedIn()) {
 		window.location.href = '/home.html';
+	}
+	if (buttonPressed == "todo") {
+		window.location.href = '/todo.html';
+	}
+	if (buttonPressed == "goals") {
+		window.location.href = '/goals.html';
+	}
+	if (buttonPressed == "habits") {
+		window.location.href = '/habits.html';
+	}
+	if (buttonPressed == "home") {
+		window.location.href = 'home.html';
+	}
+}
+
+rhit.initalizePage = function () {
+	console.log("INITIALIZATION");
+	const urlParams = new URLSearchParams(window.location.search);
+	if (document.querySelector("#homePage")) {
+		const uid = urlParams.get("uid");
+		new rhit.HomePageController();
+	}
+	if (document.querySelector("#todoPage")) {
+		new rhit.TodoPageController();
+	}
+	if (document.querySelector("#goalsPage")) {
+		new rhit.GoalsPageController();
 	}
 }
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
+	console.log("ready");
+	if (document.querySelector(".loginPage")) {
+		rhit.LoginPageController = new rhit.LoginPageController();
+		console.log("Login Page Initialized");
+		rhit.fbAuthManager = new rhit.fbAuthManager();
+		console.log("Auth Manager Initalized");
+	}
+	if (document.querySelector("#homePage")) {
+		new rhit.HomePageController();
+	}
 }
 
 rhit.main();
