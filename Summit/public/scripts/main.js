7771201eb5ee5c3a91ae9a505232ecc45b516ca0
rhit-hamerfg <hamerfg@rose-hulman.edu>
2024-02-13 11:31:41 -0500

    login page js

--- Summit/public/scripts/main.js
+++ Summit/public/scripts/main.js
@@ -10,179 +10,121 @@
 
 var rhit = rhit || {};
 
+rhit.LoginPageController = class {
+	constructor() {
+		const inputEmail = document.querySelector("#inputEmail");
+		const inputPassword = document.querySelector("#inputPassword");
+		const inputName = document.querySelector("#inputName");
 
+		document.querySelector("#createAccountButton").onclick = (event) => {
+			console.log(`Create account for email: ${inputEmail.value} password: ${inputPassword.value} name ${inputName.value}`);
+			firebase.auth().createUserWithEmailAndPassword(inputEmail.value, inputPassword.value).catch(function (error) {
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				// ..
+				console.log("Account creation error.");
+			});
+		}
+
+		document.querySelector("#loginButton").onclick = (event) => {
+			console.log(`Log in for email: ${inputEmail.value} password: ${inputPassword.value}`);
+	
+			firebase.auth().signInWithEmailAndPassword(inputEmail.value, inputPassword.value).catch(function (error) {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				console.log("Login error.");
+			});
+		};
+	}
+}
 
 rhit.fbAuthManager = class {
 	constructor() {
 		this._user = null;
 	}
+
 	beginListening(changeListener) {
 		firebase.auth().onAuthStateChanged((user) => {
 			this._user = user;
 			changeListener();
-		})
-	}
-}
-
-rhit.LoginPageController = class {
-	constructor() {
-
-		this._user = null;
-		this._unsubscribe = null;
-
-		const inputEmail = document.querySelector("#inputEmail");
-		const inputPassword = document.querySelector("#inputPassword");
-
-		document.querySelector("#loginButton").onclick = (event) => {
-			console.log(`Login with ${inputEmail.value}, ${inputPassword.value}`);
-			firebase.auth().signInWithEmailAndPassword(inputEmail.value, inputPassword.value).then((user) => {
-				this._user = user;
-				console.log(this._user);
-				console.log(this.isSignedIn());
-				rhit.checkForRedirects(null);
-			});
-		}
-
-		document.querySelector("#createAccountButton").onclick = (event) => {
-			console.log(`Create account for ${inputEmail.value}, ${inputPassword.value}`);
-			firebase.auth().createUserWithEmailAndPassword(inputEmail.value, inputPassword.value)
-		}
+		});
 	}
 
-	isSignedIn() {
+	signOut() {
+		firebase.auth().signOut();
+	}
+	get isSignedIn() {
 		return !!this._user;
 	}
-
-	stopListening(changeListener) {
-		this._unsubscribe();
+	get uid() {
+		return this._user.uid;
 	}
 }
 
-rhit.HomePageController = class {
-	constructor() {
-		document.querySelector("#todoButton").onclick = (event) => {
-			console.log("Going to TODO page");
-			rhit.checkForRedirects("todo");
-			rhit.initalizePage();
-		};
-		document.querySelector("#goalsButton").onclick = (event) => {
-			console.log("Going to GOALS page");
-			rhit.checkForRedirects("goals");
-			rhit.initalizePage();
-		};
-		document.querySelector("#habitsButton").onclick = (event) => {
-			console.log("Going to HABITS page");
-			rhit.checkForRedirects("habits");
-			rhit.initalizePage();
-		};
-		document.querySelector("#summitLogoText").onclick = (event) => {
-			console.log("Going to HOME page");
-			rhit.checkForRedirects("home");
-			rhit.initalizePage();
-		};
+rhit.checkForRedirects = function () {
+	if (document.querySelector(".loginPage") && !rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/";
 	}
-
-}
-
-rhit.TodoPageController = class {
-	constructor() {
-		document.querySelector("#todoButton").onclick = (event) => {
-			console.log("Going to TODO page");
-			rhit.checkForRedirects("todo");
-			rhit.initalizePage();
-		};
-		document.querySelector("#goalsButton").onclick = (event) => {
-			console.log("Going to GOALS page");
-			rhit.checkForRedirects("goals");
-			rhit.initalizePage();
-		};
-		document.querySelector("#habitsButton").onclick = (event) => {
-			console.log("Going to HABITS page");
-			rhit.checkForRedirects("habits");
-			rhit.initalizePage();
-		};
-		document.querySelector("#summitLogoText").onclick = (event) => {
-			console.log("Going to HOME page");
-			rhit.checkForRedirects("home");
-			rhit.initalizePage();
-		};
+	if (document.querySelector(".loginPage") && rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/home.html";
 	}
-
-}
-
-rhit.GoalsPageController = class {
-	constructor() {
-		document.querySelector("#todoButton").onclick = (event) => {
-			console.log("Going to TODO page");
-			rhit.checkForRedirects("todo");
-			rhit.initalizePage();
-		};
-		document.querySelector("#goalsButton").onclick = (event) => {
-			console.log("Going to GOALS page");
-			rhit.checkForRedirects("goals");
-			rhit.initalizePage();
-		};
-		document.querySelector("#habitsButton").onclick = (event) => {
-			console.log("Going to HABITS page");
-			rhit.checkForRedirects("habits");
-			rhit.initalizePage();
-		};
-		document.querySelector("#summitLogoText").onclick = (event) => {
-			console.log("Going to HOME page");
-			rhit.checkForRedirects("home");
-			rhit.initalizePage();
-		};
+	if (!document.querySelector(".homePage") && !rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/index.html";
 	}
+};
 
-}
-
-
-rhit.checkForRedirects = function (buttonPressed) {
-	if (document.querySelector(".loginPage") && rhit.LoginPageController.isSignedIn()) {
-		window.location.href = '/home.html';
-	}
-	if (buttonPressed == "todo") {
-		window.location.href = '/todo.html';
-	}
-	if (buttonPressed == "goals") {
-		window.location.href = '/goals.html';
-	}
-	if (buttonPressed == "habits") {
-		window.location.href = '/habits.html';
-	}
-	if (buttonPressed == "home") {
-		window.location.href = 'home.html';
-	}
-}
-
-rhit.initalizePage = function () {
-	console.log("INITIALIZATION");
+rhit.initializePage = function () {
 	const urlParams = new URLSearchParams(window.location.search);
-	if (document.querySelector("#homePage")) {
-		const uid = urlParams.get("uid");
-		new rhit.HomePageController();
+	if (document.querySelector(".loginPage")) {
+		new rhit.LoginPageController();
 	}
-	if (document.querySelector("#todoPage")) {
-		new rhit.TodoPageController();
-	}
-	if (document.querySelector("#goalsPage")) {
-		new rhit.GoalsPageController();
-	}
+// 	if (document.querySelector(".loginPage")) {
+// 		// const imageId = rhit.storage.getImageId();
+// 		const imageId = urlParams.get("id");
+// 		if (!imageId) {
+// 			window.location.href = '/';
+// 		}
+// 		rhit.fbSingleImageManager = new rhit.FbSingleImageManager(imageId);
+// 		new rhit.DetailPageController();
+// 	}
+// 	if (document.querySelector("#detailPage")) {
+// 		// const imageId = rhit.storage.getImageId();
+// 		const imageId = urlParams.get("id");
+// 		if (!imageId) {
+// 			window.location.href = '/';
+// 		}
+// 		rhit.fbSingleImageManager = new rhit.FbSingleImageManager(imageId);
+// 		new rhit.DetailPageController();
+// 	}
+// 	if (document.querySelector("#detailPage")) {
+// 		// const imageId = rhit.storage.getImageId();
+// 		const imageId = urlParams.get("id");
+// 		if (!imageId) {
+// 			window.location.href = '/';
+// 		}
+// 		rhit.fbSingleImageManager = new rhit.FbSingleImageManager(imageId);
+// 		new rhit.DetailPageController();
+// 	}
+// 	if (document.querySelector("#detailPage")) {
+// 		// const imageId = rhit.storage.getImageId();
+// 		const imageId = urlParams.get("id");
+// 		if (!imageId) {
+// 			window.location.href = '/';
+// 		}
+// 		rhit.fbSingleImageManager = new rhit.FbSingleImageManager(imageId);
+// 		new rhit.DetailPageController();
+// 	}
 }
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("ready");
-	if (document.querySelector(".loginPage")) {
-		rhit.LoginPageController = new rhit.LoginPageController();
-		console.log("Login Page Initialized");
-		rhit.fbAuthManager = new rhit.fbAuthManager();
-		console.log("Auth Manager Initalized");
-	}
-	if (document.querySelector("#homePage")) {
-		new rhit.HomePageController();
-	}
+	rhit.fbAuthManager = new rhit.fbAuthManager();
+	rhit.fbAuthManager.beginListening(() => {
+		rhit.checkForRedirects();
+		rhit.initializePage();
+	})
 }
 
 rhit.main();
